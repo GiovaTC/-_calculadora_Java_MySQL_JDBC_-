b898537b362a7e1213a44c51039f0c0ae8128e60
Giovanny Alejandro Tapiero Cataño <giovanny.tapiero@pi.edu.co>
2026-07-08 10:27:42 -0500

    commit - OperacionDAO / .

--- src/main/java/com/ejemplo/calculadora/Calculadora.java
+++ src/main/java/com/ejemplo/calculadora/Calculadora.java
@@ -17,4 +17,4 @@ public class Calculadora {
     public double dividir(double a, double b) {
         return a/b;
     }
-}   
+}
--- src/main/java/com/ejemplo/calculadora/OperacionDAO.java
+++ src/main/java/com/ejemplo/calculadora/OperacionDAO.java
@@ -1,4 +1,66 @@
 package com.ejemplo.calculadora;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
 public class OperacionDAO {
+
+    public void guardar(Operacion op) {
+        try(Connection con = Conexion.conectar()) {
+
+            String sql =
+                    "insert into operaciones(numero1,numero2,operacion,resultado) values(?,?,?,?)";
+
+            PreparedStatement ps = con.prepareStatement(sql);
+
+            ps.setDouble(1, op.getNumero1());
+            ps.setDouble(2, op.getNumero2());
+            ps.setString(3, op.getOperacion());
+            ps.setDouble(4, op.getResultado());
+
+            ps.executeUpdate();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void listar() {
+
+        try(Connection con = Conexion.conectar()){
+
+            String sql="select * from operaciones";
+
+            PreparedStatement ps=con.prepareStatement(sql);
+
+            ResultSet rs=ps.executeQuery();
+
+            System.out.println();
+
+            System.out.println("HISTORIAL");
+
+            while(rs.next()){
+
+                System.out.println(
+
+                        rs.getInt("id")+" | "+
+
+                                rs.getDouble("numero1")+" | "+
+
+                                rs.getString("operacion")+" | "+
+
+                                rs.getDouble("numero2")+" = "+
+
+                                rs.getDouble("resultado")
+
+                );
+
+            }
+
+        }catch(Exception e){
+
+            e.printStackTrace();
+        }
+    }   
 }
