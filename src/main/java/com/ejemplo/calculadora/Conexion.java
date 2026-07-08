9cf070f6c542fdd55c1e488d43655a84bbeb29ed
Giovanny Alejandro Tapiero Cataño <giovanny.tapiero@pi.edu.co>
2026-07-08 10:14:38 -0500

    commit - Conexion / .

--- src/main/java/com/ejemplo/calculadora/Conexion.java
+++ src/main/java/com/ejemplo/calculadora/Conexion.java
@@ -1,4 +1,26 @@
 package com.ejemplo.calculadora;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Conexion {
+
+    private static final String URL =
+            "jdbc:mysql://localhost:3306/calculadora";
+
+    private static final String USER = "root";
+    private static final String PASSWORD = "Tapiero123";
+
+    public static Connection conectar() {
+
+        try {
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+
+        } catch (Exception e) {
+
+            e.printStackTrace();
+            return null;
+        }
+    }   
 }
